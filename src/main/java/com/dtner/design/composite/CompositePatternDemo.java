e866b74dde98a1c88522e9469c95bfee17f6721d
dt <245548212@qq.com>
2020-07-30 17:41:39 +0800

    组合模式

---
+++ src/main/java/com/dtner/design/composite/CompositePatternDemo.java
@@ -0,0 +1,18 @@
+package com.dtner.design.composite;
+
+/**
+ * @ClassName CompositePatternDemo
+ * @Description: 组合模式 demo
+ * @Author dt
+ * @Date 2020-07-30
+ **/
+public class CompositePatternDemo {
+
+    public static void main(String[] args) {
+
+        Employee ceo = new Employee("John","CEO",30000);
+        Employee headSales = new Employee("Michel","Head Marketing",300);
+
+    }
+
+}
---
+++ src/main/java/com/dtner/design/composite/Employee.java
@@ -0,0 +1,60 @@
+package com.dtner.design.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName Employee
+ * @Description:
+ * @Author dt
+ * @Date 2020-07-30
+ **/
+public class Employee {
+
+    private String name;
+
+    private String dept;
+
+    private int salary;
+
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.subordinates = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(List<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
+}
