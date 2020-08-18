f02c1e093e493b10a04752842529e9a0d62a1d20
245548212@qq.com <a1311706>
2020-08-18 16:22:53 +0800

    解释器模式

---
+++ src/main/java/com/dtner/design/interpreter/impl/TerminalExpression.java
@@ -0,0 +1,20 @@
+package com.dtner.design.interpreter.impl;
+
+import com.dtner.design.interpreter.iter.Expression;
+
+public class TerminalExpression implements Expression {
+
+    private String data;
+
+    public TerminalExpression(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean interpret(String context) {
+        if(context.contains(data)){
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/com/dtner/design/interpreter/iter/Expression.java
@@ -0,0 +1,7 @@
+package com.dtner.design.interpreter.iter;
+
+public interface Expression {
+
+    public boolean interpret(String context);
+
+}
